df6b455de79d0d9f3042ad53af0e01a0527a8a07
Zolics <153197257+Zolicsiz@users.noreply.github.com>
2024-01-03 20:54:38 +0300

    First Commit
    Load Files

---
+++ AutoAlphaWarhead/Config.cs
@@ -0,0 +1,20 @@
+﻿using Exiled.API.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoAlphaWarhead
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = false;
+        [Description("Time when Warhead will be activating")]
+        public int WarheadTime { get; set; } = 1200;
+        [Description("Text which will be showing when auto warhead activated")]
+        public string Text { get; set; } = "<color=red>Auto Warhead was activating</color>";
+    }
+}
---
+++ AutoAlphaWarhead/Plugin.cs
@@ -0,0 +1,56 @@
+﻿using Exiled.API.Features;
+using MEC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoAlphaWarhead
+{
+    public class Plugin : Plugin<Config>
+    {
+        public static Plugin plugin;
+        public override string Author => "Zolics";
+        public override string Prefix => "auto_warhead";
+        public override string Name => "AutoWarhead";
+        public CoroutineHandle handle = default(CoroutineHandle);
+        public int TimeStart = 0;
+        public override void OnEnabled()
+        {
+            Exiled.Events.Handlers.Server.RoundStarted += OnRoundStarted;
+            Exiled.Events.Handlers.Server.WaitingForPlayers += OnWaitingForPlayers;
+            base.OnEnabled();
+        }
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Server.RoundStarted -= OnRoundStarted;
+            Exiled.Events.Handlers.Server.WaitingForPlayers -= OnWaitingForPlayers;
+            base.OnDisabled();
+        }
+        public void OnWaitingForPlayers()
+        {
+            TimeStart = 0;
+            Timing.KillCoroutines(handle);
+        }
+        public void OnRoundStarted()
+        {
+            handle = Timing.RunCoroutine(WarheadHandle());
+        }
+        public IEnumerator<float> WarheadHandle()
+        {
+            while(Round.IsStarted)
+            {
+                TimeStart++;
+                if(TimeStart == Plugin.plugin.Config.WarheadTime)
+                {
+                    Map.Broadcast(10, Plugin.plugin.Config.Text);
+                    Warhead.DetonationTimer = 90f;
+                    Warhead.IsLocked = true;
+                    Warhead.Start();
+                }
+                yield return Timing.WaitForSeconds(1f);
+            }
+        }
+    }
+}
